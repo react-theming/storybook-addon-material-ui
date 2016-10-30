66805ac63fc780dca7059a2a8fb4d376c38d82cf
minibhati93 <minibhati93@gmail.com>
2016-10-30 12:36:06 +0530

    ui improvements

--- example/LoremMaterial/MaterialLoginPage.jsx
+++ example/LoremMaterial/MaterialLoginPage.jsx
@@ -1,5 +1,14 @@
 import React from 'react';
 import TextField from 'material-ui/TextField';
+import { Card, CardHeader, CardText } from 'material-ui/Card';
+import RaisedButton from 'material-ui/RaisedButton';
+import {orange500, blue500} from 'material-ui/styles/colors';
+
+const styles = {
+  errorStyle: {
+    color: blue500,
+  },
+};
 
 export default class MaterialAppExampleProgress extends React.Component {
     constructor(props) {
@@ -13,54 +22,36 @@ export default class MaterialAppExampleProgress extends React.Component {
         return (
           <div
           style={{
-                margin: '20px 100px',
-                minWidth: 400,
-                maxWidth: 800,
-            }}
+              margin: '20px 100px',
+              minWidth: 400,
+              maxWidth: 450,
+          }}
           >
-            <TextField
-              hintText="Hint Text"
-            /><br />
-            <br />
-            <TextField
-              hintText="The hint text can be as long as you want, it will wrap."
-            /><br />
-            <TextField
-              id="text-field-default"
-              defaultValue="Default Value"
-            /><br />
-            <TextField
-              hintText="Hint Text"
-              floatingLabelText="Floating Label Text"
-            /><br />
-            <TextField
-              hintText="Hint Text"
-              floatingLabelText="Fixed Floating Label Text"
-              floatingLabelFixed={true}
-            /><br />
-            <TextField
-              hintText="Password Field"
-              floatingLabelText="Password"
-              type="password"
-            /><br />
-            <TextField
-              hintText="MultiLine with rows: 2 and rowsMax: 4"
-              multiLine={true}
-              rows={2}
-              rowsMax={4}
-            /><br />
-            <TextField
-              hintText="Message Field"
-              floatingLabelText="MultiLine and FloatingLabel"
-              multiLine={true}
-              rows={2}
-            /><br />
-            <TextField
-              hintText="Full width"
-              fullWidth={true}
+          <Card style={{ width: '100%' }} >
+            <CardHeader
+              title="Login Page"
             />
-          </div>
-        );
+            <CardText>
+                <TextField
+                hintText="Email Address"
+                errorText="This field is required"
+                hintStyle={styles.errorStyle}
+              /><br />
+              <br />
+              <TextField
+                hintText="Password"
+                type="password"
+                errorText="This field is required"
+                hintStyle={styles.errorStyle}
+              /><br />
+              <div style={{justifyContent: 'center', display: 'flex'}}>
+                <RaisedButton label="Login" primary={true} />
+                <RaisedButton label="Sign Up" secondary={true} />
+              </div>
+          </CardText>
+          </Card>
+        </div>
+      );
     }
 }
 
