f5a0f3e699be25d9bdf008be5e8157596c0c6c62
Oleg Proskurin <regx@usul.su>
2018-06-10 12:08:58 +0300

    add survey

--- src/components/ThemePanel.jsx
+++ src/components/ThemePanel.jsx
@@ -3,9 +3,15 @@ import PropTypes from 'prop-types';
 import Menu from '@material-ui/core/Menu';
 import Button from '@material-ui/core/Button';
 import MenuItem from '@material-ui/core/MenuItem';
+import Icon from '@material-ui/core/Icon';
 import IconNew from '@material-ui/icons/CreateNewFolder';
 import IconDnLoad from '@material-ui/icons/FileDownload';
 import IconClean from '@material-ui/icons/DeleteForever';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
 
 import SvgButton from '../material-desktop/SvgButton';
 import SclToggle from '../material-desktop/SclToggle';
@@ -75,7 +81,8 @@ export default class ThemePanel extends React.Component {
                 width: '100%',
                 display: 'flex',
                 justifyContent: 'space-between',
-                flexWrap: 'wrap',
+                flexDirection: 'row',
+                    // flexWrap: 'wrap',
                 backgroundColor: 'white' /* this.context.muiTheme.palette.canvasColor*/,
             }}
           >
@@ -87,6 +94,8 @@ export default class ThemePanel extends React.Component {
                         // justifyContent: 'space-between',
                   padding: 16,
                   flexWrap: 'wrap',
+                  width: 300,
+                  flexGrow: 1,
               }}
             >
               {this.props.themesNameList.map((name, ind) => (
@@ -95,11 +104,55 @@ export default class ThemePanel extends React.Component {
                   key={name}
                   onClick={() => this.props.onThemeSelect(ind)}
                   color="primary"
-                  style={{ margin: 4 }}
+                  style={{ margin: 4, maxWidth: 300 }}
                 >
                   {`${name}`}
                 </Button>
                     ))}
+            </div>
+            <div
+              style={{
+                  minWidth: 160,
+                  display: 'flex',
+                  flexDirection: 'column',
+                        // justifyContent: 'space-between',
+                  flexWrap: 'wrap',
+                  width: 300,
+                  padding: 8,
+              }}
+            >
+              <Card style={{ height: '100%' }}>
+                <a
+                  href="https://app.qpointsurvey.com/s.aspx?c=F2VOSpTXOlnHHqMaZKSSV5a1ylaCDoRfhut3oNCox34~"
+                  target="blank"
+                  style={{ textDecoration: 'none' }}
+                >
+                  <CardContent>
+                    <Typography color="primary">
+                      <b>Material-UI v1 support announcement</b>
+                    </Typography>
+                    <Divider style={{ marginBottom: 16 }} />
+                    <Typography color="textSecondary" style={{ fontSize: 12 }}>
+                                    Since v.1 themes have the completely new structure, we need to
+                                    change a lot in this addon. Let us know <b>what features</b> are
+                                    most needed to you.
+                                </Typography>
+                    <Typography color="textSecondary" style={{ fontSize: 12 }}>
+                                    We would appreciate if you give us your <b>feedback</b> via this
+                                    survey. It is completely anonymous and contains only 10
+                                    questions. It'll help us to provide you with the best DX.
+                                </Typography>
+                    <Typography style={{ marginTop: 16 }} align="center">
+                      <Button
+                        // href="https://app.qpointsurvey.com/s.aspx?c=F2VOSpTXOlnHHqMaZKSSV5a1ylaCDoRfhut3oNCox34~"
+                        target="blank"
+                      >
+                                        complete this poll
+                                    </Button>
+                    </Typography>
+                  </CardContent>
+                </a>
+              </Card>
             </div>
             {/* <div style={{ width: 200, minWidth: 150, flexGrow: 1, padding: 16 }} >
               <textarea
