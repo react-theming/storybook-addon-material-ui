9a2bbbcffad14b35944eeb6cc054eaaee70939de
Oleg Proskurin <regx@usul.su>
2018-09-17 11:37:43 +0300

    fix compatibility issue

--- src/register.js
+++ src/register.js
@@ -6,7 +6,7 @@ import { ADDON_ID, PANEL_ID } from './config';
 addons.register(ADDON_ID, api => {
   addons.addPanel(PANEL_ID, {
     title: 'Material-UI',
-    render: ({ active }) => (
+    render: ({ active } = {}) => (
       <AddonPanel api={api} active={active} panel pointName="addon panel" />
     )
   });
