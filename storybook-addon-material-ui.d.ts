0d4c7cbbf9831a6ac81008dbbf71e1f362e86dfb
carlos <echoulen@gmail.com>
2020-01-05 00:35:35 +0800

    specific the type of theme

--- storybook-addon-material-ui.d.ts
+++ storybook-addon-material-ui.d.ts
@@ -1,3 +1,4 @@
 import {DecoratorFunction} from "@storybook/addons";
+import {Theme} from "@material-ui/core";
 
-export declare function muiTheme<T = {}>(arg?: Array<any>): DecoratorFunction<T>;
+export declare function muiTheme<T = {}>(arg?: Array<Theme>): DecoratorFunction<T>;
