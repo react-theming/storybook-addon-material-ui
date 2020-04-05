050e249601f6c62b04c221a83f54d6cd47e805d7
Oleg Proskurin <regx@usul.su>
2020-04-05 16:38:04 +0300

    Update dependencies

--- .storybook/stories.js
+++ .storybook/stories.js
@@ -3,18 +3,19 @@ import React from 'react';
 import { ThemeProvider } from 'emotion-theming';
 
 import { storiesOf } from '@storybook/react';
-import { useThemes, createTheme } from '../src/index.js';
+import { withThemes, createTheme } from '../src/index.js';
 
 import { Button } from './ui';
 import { theme, themeAlt, darkTheme } from './theme';
 
-const setThemes = useThemes(ThemeProvider, [
-  theme,
-  // themeAlt,
-  // darkTheme,
-]);
+// const setThemes = useThemes(ThemeProvider, [
+//   theme,
+//   // themeAlt,
+//   // darkTheme,
+// ]);
 
 storiesOf('Button', module)
+  .addDecorator(withThemes(ThemeProvider, [theme]))
   .add(
     'Buttons1',
     () => <Button>Hello Button</Button>,
--- src/manager/UI/Toolbar.styled.js
+++ src/manager/UI/Toolbar.styled.js
@@ -1,8 +1,7 @@
 import { styled } from '@storybook/theming';
 
 export const Container = styled.div`
-  background-color: ${({ theme }) =>
-    console.log('theme', theme) || theme.background.app};
+  background-color: ${({ theme }) => theme.background.app};
 
   padding: 4px 8px;
   display: flex;
