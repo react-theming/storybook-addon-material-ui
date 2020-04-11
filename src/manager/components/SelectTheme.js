60b5499e86d136fb358821b8fcf0961728088567
Oleg Proskurin <regx@usul.su>
2020-04-12 02:05:24 +0300

    Add Material Swatch adaptor

--- src/manager/components/SelectTheme.js
+++ src/manager/components/SelectTheme.js
@@ -5,6 +5,17 @@ import * as styled from './SelectTheme.styled';
 import Toolbar from '../UI/Toolbar';
 import Caption from '../UI/Caption';
 
+const materialPreview = ({ palette }) => ({
+  main: [palette.primary.main, palette.primary.light, palette.primary.dark],
+  text: [palette.text.secondary],
+  accent: [
+    palette.secondary.main,
+    palette.secondary.light,
+    palette.secondary.dark,
+  ],
+  background: [palette.text.primary],
+});
+
 const SelectTheme = ({ themeInfoList, themeInd, setCurrent }) => {
   if (!themeInfoList) return 'No themes info';
   const count = themeInfoList.length;
@@ -18,8 +29,17 @@ const SelectTheme = ({ themeInfoList, themeInd, setCurrent }) => {
       <styled.ListHolder>
         <ul>
           {themeInfoList.map(({ name, theme }, ind) => {
-            const { flattenColors } = flattenTheme(theme);
-            const colorList = flattenColors.map(({ original }) => original);
+            let colorList;
+            if (
+              theme.palette &&
+              theme.palette.primary &&
+              theme.palette.primary.main
+            ) {
+              colorList = materialPreview(theme);
+            } else {
+              const { flattenColors } = flattenTheme(theme);
+              colorList = flattenColors.map(({ original }) => original);
+            }
             return (
               <li key={name}>
                 <styled.Theme
